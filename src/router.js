bdc714c79948251a1f09cf784e72088a71ddbe55
exorcist <yag0da@yandex.ru>
2019-11-28 00:36:52 +0300

    fix

--- src/router.js
+++ src/router.js
@@ -7,8 +7,10 @@ const redis = require('./redis')
 const router = new Router();
 
 router.post('/click', async (ctx) => {
-  const { clicks, hash } = ctx.request.body;
-  const count = clicks >= 1000 ? `${((clicks + 1) / 1000).toFixed(1)}k` : (clicks + 1)
+  const clicks = +ctx.request.body.clicks + 1;
+  const hash = ctx.request.body.hash;
+
+  const count = clicks >= 1000 ? `${(clicks / 1000).toFixed(1)}k` : clicks
   const messageData = JSON.parse(await redis.get(`${config.redisPrefix}${hash}`));
   try {
     await bot.telegram.editMessageReplyMarkup(messageData.channelId, messageData.messageId, messageData.inlineMessageId, Markup.inlineKeyboard([Markup.urlButton(`🔥КУПИТЬ🔥 ${count}`, messageData.url)]))
