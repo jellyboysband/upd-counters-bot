1dceaf6160bc14ff789a8f3e83741d587c28c75c
exorcist <yag0da@yandex.ru>
2019-11-28 00:31:52 +0300

    fix

--- src/router.js
+++ src/router.js
@@ -8,16 +8,13 @@ const router = new Router();
 
 router.post('/click', async (ctx) => {
   const { clicks, hash } = ctx.request.body;
-  const count = clicks >= 1000 ? `${(t / 1000).toFixed(1)}k` : clicks
+  const count = clicks >= 1000 ? `${((clicks + 1) / 1000).toFixed(1)}k` : (clicks + 1)
   const messageData = JSON.parse(await redis.get(`${config.redisPrefix}${hash}`));
-  await bot.telegram.editMessageReplyMarkup(messageData.channelId, messageData.messageId, messageData.inlineMessageId, Markup.inlineKeyboard([Markup.urlButton(`🔥КУПИТЬ🔥 ${count}`, messageData.url)]))
-
-  if (!messageId) {
-    ctx.throw(404, 'not found');
-  } else {
-
+  try {
+    await bot.telegram.editMessageReplyMarkup(messageData.channelId, messageData.messageId, messageData.inlineMessageId, Markup.inlineKeyboard([Markup.urlButton(`🔥КУПИТЬ🔥 ${count}`, messageData.url)]))
+  } catch (err) {
+    console.error(err)
   }
-
 });
 
 
