e655648cef32ae104bbf87c6c23879a5b16b3de0
prilipko@novotorica.com <prilipko@novotorica.com>
2019-11-28 10:34:55 +0300

    like =❤️

--- src/router.js
+++ src/router.js
@@ -2,28 +2,32 @@ const Router = require('koa-router');
 const config = require('./config');
 const { bot } = require('./bot');
 const Markup = require('telegraf/markup');
-const redis = require('./redis')
+const redis = require('./redis');
 
 const router = new Router();
 
-router.post('/click', async (ctx) => {
+router.post('/click', async ctx => {
   const clicks = +ctx.request.body.clicks + 1;
   const hash = ctx.request.body.hash;
 
   if (clicks > 1000 && click % 100 !== 0) {
     ctx.status = 200;
-    return
+    return;
   }
 
-  const count = clicks >= 1000 ? `${(clicks / 1000).toFixed(1)}k` : clicks
+  const count = clicks >= 1000 ? `${(clicks / 1000).toFixed(1)}k` : clicks;
   const messageData = JSON.parse(await redis.get(`${config.redisPrefix}${hash}`));
   try {
-    await bot.telegram.editMessageReplyMarkup(messageData.channelId, messageData.messageId, messageData.inlineMessageId, Markup.inlineKeyboard([Markup.urlButton(`🔥КУПИТЬ🔥 ${count}`, messageData.url)]))
+    await bot.telegram.editMessageReplyMarkup(
+      messageData.channelId,
+      messageData.messageId,
+      messageData.inlineMessageId,
+      Markup.inlineKeyboard([Markup.urlButton(`🔥КУПИТЬ🔥 ${count}❤️`, messageData.url)]),
+    );
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
   ctx.status = 200;
 });
-
 
 module.exports = router.routes();
